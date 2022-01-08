1ea4fc0d0e99ae8f6232ce22b2dad9d0211dbb39
Alexandre Normand <alexandre.normand@gmail.com>
2022-01-08 14:09:51 -0800

    Increase timeout to connect device

--- legohat/motor.go
+++ legohat/motor.go
@@ -89,7 +89,7 @@ func (l *LegoHatMotorDriver) Start() (err error) {
 }
 
 func (l *LegoHatMotorDriver) waitForConnect() (err error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
 	for _, d := range l.devices {
