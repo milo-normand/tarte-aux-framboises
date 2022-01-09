30b14beb479e2b7097eaef88ed22644de6c60d8b
Alexandre Normand <alexandre.normand@gmail.com>
2022-01-08 16:20:47 -0800

    Bootstrap Legocar

---
+++ legocar/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/milo-normand/tarte-aux-framboises/legohat"
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/platforms/joystick"
+	"gobot.io/x/gobot/platforms/raspi"
+)
+
+const (
+	maxAngle = 60
+	maxSpeed = 100
+)
+
+func main() {
+	r := raspi.NewAdaptor()
+	hat := legohat.NewAdaptor(r)
+	motor := legohat.NewLegoMotorDriver(hat, legohat.PortA)
+	direction := legohat.NewLegoMotorDriver(hat, legohat.PortB)
+	joystickAdaptor := joystick.NewAdaptor()
+	ctrl := joystick.NewDriver(joystickAdaptor, "dualshock4")
+
+	work := func() {
+		log.Printf("Started lego hat")
+
+		ctrl.On(joystick.LeftY, func(data interface{}) {
+			fmt.Println("left_y", data)
+			if val, ok := data.(int16); !ok {
+				log.Printf("error reading int16 value from %v\n", data)
+			} else {
+				degree := int(float64(val) / 32768.0 * float64(maxAngle))
+				log.Printf("Adjusting front motor to degree %d", degree)
+
+				direction.RunForDegrees(degree)
+			}
+		})
+		ctrl.On(joystick.RightX, func(data interface{}) {
+			fmt.Println("right_x", data)
+			if val, ok := data.(int16); !ok {
+				log.Printf("error reading int16 value from %v\n", data)
+			} else {
+				speed := int(float64(val) / 32768.0 * float64(maxSpeed))
+				log.Printf("Adjusting speed motor to %d", speed)
+
+				if speed == 0 {
+					motor.TurnOff()
+				} else {
+					motor.TurnOn(speed)
+				}
+			}
+		})
+	}
+
+	robot := gobot.NewRobot("legocar",
+		[]gobot.Connection{r, hat, joystickAdaptor},
+		[]gobot.Device{motor, ctrl},
+		work,
+	)
+
+	robot.Start()
+}
